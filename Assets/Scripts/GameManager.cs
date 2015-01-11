f9044bbce0fcb2b0dc4f3c68280ae4f260e5d390
Charles J Hardy <chuckjhardy@gmail.com>
2015-01-11 16:05:30 +0000

    Restart after Life Lost

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,18 +3,32 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
+	public static GameManager instance = null;
+
+	public float restartDelay = 3f;
+
+	public int totalLives = 3;
+	public int livesLeft = 3;
 
 	public GameObject startText;
+	public GameObject bricksPrefab;
+	public GameObject paddlePrefab;
 
-	public static GameManager instance = null;
+	public Text livesText;
+
 	private bool startKeyHit = false;
+	private GameObject bricksClone;
+	private GameObject paddleClone;
 
 	void Start () {
 		if (instance == null || instance != this) { instance = this; }
+
+		Initialize();
 	}
 	
-	void Update () {
-	
+	void Initialize () {
+		bricksClone = Instantiate (bricksPrefab, transform.position, Quaternion.identity) as GameObject;
+		paddleClone = Instantiate (paddlePrefab, new Vector2 (0, -9), Quaternion.identity) as GameObject;
 	}
 
 	public bool StartGame() {
@@ -29,5 +43,19 @@ public class GameManager : MonoBehaviour {
 		}
 
 		return false;
+	}
+
+	public void Lost () {
+		livesLeft--;
+		livesText.text = livesLeft + "/" + totalLives;
+
+		Reset ();
+
+		Invoke ("Initialize", restartDelay);
+	}
+
+	private void Reset () {
+		Destroy (bricksClone);
+		Destroy (paddleClone);
 	}
 }
---
+++ Assets/Scripts/LostTrigger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LostTrigger : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+		GameManager.instance.Lost ();
+	}
+
+}
